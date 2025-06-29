395d4844bfdd47255e30727e538b76bc21e593ea
kouma-root <parfaitkoumakang@gmail.com>
2025-06-29 12:52:23 +0100

    Adding file for creating a new database

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root'
+)
+
+mycursor = mydb.cursor()
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+except Exception as e:
+    print(e)
+else:
+    print("Database 'alx_book_store' created successfully!")
+mycursor.close()
+mydb.close()
